ab41e6cbfcf90fc6b00feb076b125c478013d6a8
Egorov <166631982+goganvarvar@users.noreply.github.com>
2025-05-04 19:30:15 +0300

    сщьььше

---
+++ script.js
@@ -0,0 +1,17 @@
+<script>
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
+</script>
